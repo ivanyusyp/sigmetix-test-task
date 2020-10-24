8df21abf907aef7b0e23362acfcbae0e532cd873
ivanyusyp <ivanusup@gmail.com>
2020-10-24 15:07:56 +0300

    finished test-task

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+	$("#menu-icon").click(function () {
+		$("#myNav").css("width", "100%");
+	})
+
+	$(".closebtn").click(function () {
+		$("#myNav").css("width", "0%");
+	})
+
+});
